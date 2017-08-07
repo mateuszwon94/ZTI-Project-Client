9866ff2b3dfd86a058016e6110b272ade202c1b8
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2017-08-07 18:02:39 +0200

    Add MenuPage

---
+++ ZTI-Project-Client/ZTI-Project-Client/MenuPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ZTI.Project.Client {
+	/// <summary>
+	/// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+	/// </summary>
+	public sealed partial class MenuPage : Page {
+		public MenuPage() { InitializeComponent(); }
+
+		private void HamburgerButton_OnClick(object sender, RoutedEventArgs e) =>
+			HamburgerSplitView.IsPaneOpen = !HamburgerSplitView.IsPaneOpen;
+
+		private void HamburgerListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if ( HamburgerSplitView.IsPaneOpen ) HamburgerSplitView.IsPaneOpen = false;
+
+			if ( PlayListBoxItem.IsSelected ) {
+				MainFrame.Navigate(typeof(StopsPage));
+			}
+		}
+	}
+}
